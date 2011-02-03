bf9623ed44a9fbcf4eba9805d67d20bff2e3c0c9
Chris Lamb <chris@playfire.com>
2011-02-03 15:35:05 +0000

    Add Sphinx builder for docs.
    Signed-off-by: Chris Lamb <chris@playfire.com>

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'django-bcrypt'
+version = ''
+release = ''
+copyright = '2010, 2011 UUMC Ltd.'
+
+html_logo = 'playfire.png'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+html_title = "%s documentation" % project
+master_doc = 'index'
+exclude_trees = ['_build']
+templates_path = ['_templates']
+latex_documents = [
+  ('index', '%s.tex' % project, html_title, u'Playfire', 'manual'),
+]
+intersphinx_mapping = {'http://docs.python.org/': None}
